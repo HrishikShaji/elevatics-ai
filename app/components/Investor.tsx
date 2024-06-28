f79a3f4e33ce5e4e57788c95767f9ebf120042c3
HrishikShaji <hrishikminishaji@gmail.com>
2024-06-28 23:49:33 +0530

    investor ui modified

--- app/components/Investor.tsx
+++ app/components/Investor.tsx
@@ -1,19 +1,20 @@
 "use client"
-
-import { ChangeEvent, FormEvent, useState } from "react";
+import { LuUploadCloud } from "react-icons/lu";
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
+import Spinner from "./svgs/Spinner";
+import { HiArrowRight } from "react-icons/hi2";
 
 export default function Investor() {
   const [file, setFile] = useState(null);
   const [loading, setLoading] = useState(false)
   const [description, setDescription] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null)
+  console.log(file.name)
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     setFile(event.target.files[0]);
   };
 
-  const handleDescriptionChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setDescription(event.target.value);
-  };
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
@@ -37,23 +38,24 @@ export default function Investor() {
       setLoading(false)
     }
   };
+  const handleClick = () => {
+    if (inputRef.current) {
 
+      inputRef.current.click();
+    }
+  };
   return (
     <div className="h-full w-full flex justify-center items-center">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            File:
-            <input type="file" onChange={handleFileChange} />
-          </label>
+      <form onSubmit={handleSubmit} className=" w-[50%] p-10 rounded-3xl border-2 border-gray-300">
+        <input type="file" onChange={handleFileChange} ref={inputRef} hidden />
+        <div className="w-full flex items-center justify-center h-[20vh] border-dashed border-gray-300 border-2 rounded-3xl" onClick={handleClick}>
+
+          <LuUploadCloud size={25} />
         </div>
-        <div>
-          <label>
-            Description:
-            <input type="text" value={description} onChange={handleDescriptionChange} />
-          </label>
+        <div className="w-full mt-5 flex justify-between">
+          <h1>{file.name ? file.name : null}</h1>
+          <button type="submit" className="p-2 rounded-full hover:bg-gray-300">{loading ? <div className="w-10"><Spinner /></div> : <HiArrowRight size={25} />}</button>
         </div>
-        <button type="submit">{loading ? "Uploading..." : "Upload"}</button>
       </form>
     </div>
   );
