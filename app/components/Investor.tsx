6b57ff799c6470f7e3f69970f5522e3858624073
HrishikShaji <hrishikminishaji@gmail.com>
2024-06-28 23:17:33 +0530

    investor added

--- app/components/Investor.tsx
+++ app/components/Investor.tsx
@@ -1,5 +1,60 @@
 "use client"
 
+import { ChangeEvent, FormEvent, useState } from "react";
+
 export default function Investor() {
-  return <div></div>
+  const [file, setFile] = useState(null);
+  const [loading, setLoading] = useState(false)
+  const [description, setDescription] = useState("");
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleDescriptionChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setDescription(event.target.value);
+  };
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("description", description);
+
+    try {
+      setLoading(true)
+      const response = await fetch("https://nithin1905-investor.hf.space/investor", {
+        method: "POST",
+        body: formData,
+      });
+
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      setLoading(false)
+    }
+  };
+
+  return (
+    <div className="h-full w-full flex justify-center items-center">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            File:
+            <input type="file" onChange={handleFileChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Description:
+            <input type="text" value={description} onChange={handleDescriptionChange} />
+          </label>
+        </div>
+        <button type="submit">{loading ? "Uploading..." : "Upload"}</button>
+      </form>
+    </div>
+  );
 }
