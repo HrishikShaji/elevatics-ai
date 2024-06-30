1dd42c1543e0ccd71ddd98db742e394b98d6f9dc
HrishikShaji <hrishikminishaji@gmail.com>
2024-06-30 09:05:09 +0530

    rendered other_info

--- app/investment/sample/page.tsx
+++ app/investment/sample/page.tsx
@@ -1,15 +1,26 @@
 "use client"
 
+import Slider from "@/app/components/ui/Slider"
 import { useInvestor } from "@/app/contexts/InvestorContext"
+import { useState } from "react"
 import ReactMarkdown from "react-markdown"
 
 export default function Page() {
-
+  const [currentIndex, setCurrentIndex] = useState(0)
   const { data } = useInvestor()
   if (!data) return null;
-  console.log(data)
+  const sliderData = Object.entries(data.other_info_results)
+
+  console.log(sliderData[currentIndex][1])
   return (
     <div className="pt-[200px]">
+      <Slider items={sliderData} currentIndex={currentIndex} setCurrentIndex={setCurrentIndex} />
+      <div className="h-[70vh] overflow-y-scroll custom-scrollbar flex-col px-[240px]">
+        <ReactMarkdown>
+
+          {sliderData[currentIndex][1]}
+        </ReactMarkdown>
+      </div>
       <div className="px-[240px] flex h-[80vh] gap-10 overflow-y-scroll custom-scrollbar flex-col">{Array.from({ length: data.queries.length }).map((_, i) => (
         <div className="flex bg-green-500 flex-col gap-2 " key={i}>
           <ReactMarkdown>{data.queries[i]}</ReactMarkdown>
